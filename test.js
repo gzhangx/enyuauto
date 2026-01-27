763174439e371d39f7e52a88710bf8c342a0212f
Gang Zhang <gzhangx@hotmail.com>
2026-01-26 19:40:46 -0500

    test

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 
 const mainOps = require('./lib/main_ops.js');
 
-mainOps.main().catch(err => {
+const args = process.argv.slice(2);
+let opstr;
+if (args.includes('test')) {
+    opstr = 'debug_remove_test_';
+} else if (args.includes('del')) {
+    opstr = 'del';
+}
+
+mainOps.main(opstr).catch(err => {
     console.error('Test failed:', err);
 });
