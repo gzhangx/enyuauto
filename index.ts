0544d668de1bf5d5d8e2c3a3627d4ee9f4e0f80c
Gang Zhang <gzhangx@hotmail.com>
2026-06-30 21:42:46 -0400

    clean up

--- index.ts
+++ index.ts
@@ -249,11 +249,13 @@ async function doFreedcampAction(params: { [key: string]: string; }, log: mainOp
         }
         const attachParams: ProjectTaskParams = {
           priority: 2,
-          ...params,
+          //...params,
         };
+        //don't expand params, way too many garbage
         if (params['description']) attachParams.description = params['description'];
         if (params['assignedToId']) attachParams.assigned_to_id = params['assignedToId'];
         if (params['dueDate']) attachParams.due_ts = params['dueDate'] as unknown as number;
+        if (params['due_date']) attachParams.due_date = params['due_date'];
         
         delete attachParams.start_date;
         console.log('DEBUGsubmit attachParams', attachParams)
